2e6fe582de4d858cc148160e0ee9363ad3086a4f
Andy Choens <andy.choens@acuitashealth.com>
2022-03-19 12:37:16 -0400

    Makes it possible to pass a config to ahUtils functions.
     Changes to be committed:
    	modified:   R/import_data.R
    	modified:   R/open_con.R

--- R/import_data.R
+++ R/import_data.R
@@ -1,14 +1,12 @@
 #' import_data
 #'
-#' Looks for "file" in "folder". If it exists, it attempts to run the query
-#' AFTER running validation.sql if it exists. If validation.sql exists, all rows
-#' must return 1 (TRUE) before the report query in "file" will be run. If
-#' validation.sql fails, the function will stop for the number of seconds
-#' defined in wait_time before trying to rerun validation.sql.
+#' Looks for "file" in "folder". If it exists, it attempts to run the query.
+#' The user may also pass a query directly to import_data.
 #'
 #' @param file The file where the query lives.
 #' @param folder The folder where the query lives. If validation.sql is in this
 #' folder it will be run BEFORE the sql file.
+#' @param qry A query to import, without a file.
 #' @param verbose If TRUE, this function talks a lot. Useful for debugging
 #' connection issues and not much else.
 #' @param params A LIST of parameters to alter the query.
@@ -17,20 +15,33 @@
 #'   you will receive an empty tibble.
 #'
 #' @export
-import_data <- function(file = "query.sql", folder = "sql", verbose = FALSE, params = NULL) {
-  config <- config::get()
-  qry_file <- file.path(folder, file)
+import_data <- function(file = "query.sql", folder = "sql", qry = NULL, config = NULL,  verbose = FALSE, params = NULL) {
   connect_rate <- purrr::rate_delay(pause = 30, max_times = 10)
   dbGetQueryInsistent <- purrr::insistently(
     DBI::dbGetQuery,
     rate = connect_rate)
   if (verbose) cat("\n- Connecting as: ", Sys.getenv("edw_user"))
-  con <- ahUtils::open_con()
+  if (is.null(config)) {
+    con <- ahUtils::open_con()
+  } else {
+    con <- ahUtils::open_con(config = config)
+  }
+  if (is.null(qry)) {
+    qry_file <- file.path(folder, file)
+    stopifnot(exprs = {
+      file.exists(qry_file)
+      exists("con")
+    })
+    qry <- readr::read_file(qry_file)
+  } else {
+    stopifnot(exprs = {
+      exists("con")
+    })
+  }
   stopifnot(exprs = {
-    file.exists(qry_file)
-    exists("con")
+    stringr::str_detect("select")
+    stringr::str_detect("where")
   })
-  qry <- readr::read_file(qry_file)
   if (verbose) cat("\n- Downloading data.")
   if (is.null(params)) {
     res <- dbGetQueryInsistent(con, qry)
--- R/open_con.R
+++ R/open_con.R
@@ -5,25 +5,31 @@
 #' defined. In your config.yml file, you must have dsn_name defined for both the
 #' default and rsconnect configurations.
 #'
-#' Because this function uses config.yml and .Renviron, there are NO input
-#' parameters to this function.
+#' The function can use any list with a dsn_name variable named. If not, it will
+#' open config.yml and find the dsn_name for itself.
 #'
-#' You an use dbDisconnect with this returned object because it is NO different
+#' You an use dbDisconnect with the con object because it is NO different
 #' than any other DBI connection object.
+#'
+#' @param config A configlist. If not provided, the function will open
+#'   config.yml itself.
 #' 
 #' @return A connection object.
 #'
 #' @export
-open_con <- function() {
-  config <- config::get()
+open_con <- function(config = NULL) {
+  if (is.null(config)) {
+    config <- config::get()
+  }
+  stopifnot(exprs = {
+    ## Checks for the existence of dsn_name.
+    !is.null(config$dsn_name)
+    ## Checks for the existence of our system vars.
+    Sys.getenv("edw_user") != ""
+    Sys.getenv("edw_pass")
+  })
   connect_rate <- purrr::rate_delay(pause = 30, max_times = 10)
   dbConnectInsistent <- purrr::insistently(DBI::dbConnect, rate = connect_rate)
-  ## con <- dbConnect(
-  ##   odbc::odbc(),
-  ##   dsn = config$dsn_name,
-  ##   timeout = 20,
-  ##   uid = Sys.getenv("edw_user"),
-  ##   pwd = Sys.getenv("edw_pass"))
   can_connect <-
     DBI::dbCanConnect(
       odbc::odbc(),
