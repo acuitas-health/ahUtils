c278d5b59610eb01ba20a9fd29c51d2cbcfd9c29
Andy Choens <andy.choens@acuitashealth.com>
2022-03-22 16:47:06 -0400

    Fixes some namespace bugs in report_config.
    - I failed to fully name a few functions, which caused them to fail.
    - Increments the release number.
    
     Changes to be committed:
    	modified:   DESCRIPTION
    	modified:   R/report_config.R

--- R/report_config.R
+++ R/report_config.R
@@ -13,7 +13,7 @@ report_config <- function(config = NULL, report_todo = FALSE) {
   if (is.null(config)) {
     config <- config::get()
   }
-  rc <- tibble(Config = character(), Details = character())
+  rc <- tibble::tibble(Config = character(), Details = character())
   if (!report_todo) {
     ## Only report things which exist and aren't left as TODO.
     ## This is for report.Rmd.
@@ -21,56 +21,56 @@ report_config <- function(config = NULL, report_todo = FALSE) {
       if (config$report_name != "TODO") {
         rc <-
           rc |>
-          add_row(Config = "Report Name:", Details = config$report_name)
+          tibble::add_row(Config = "Report Name:", Details = config$report_name)
       }
     }
     if (!is.null(config$report_author)) {
       if (config$report_author != "TODO") {
         rc <-
           rc |>
-          add_row(Config = "Report Author:", Details = config$report_author)
+          tibble::add_row(Config = "Report Author:", Details = config$report_author)
       }
     }
     if (!is.null(config$requested_by)) {
       if (config$requested_by != "TODO") {
         rc <-
           rc |>
-          add_row(Config = "Requested By:", Details = config$requested_by)
+          tibble::add_row(Config = "Requested By:", Details = config$requested_by)
       }
     }
     if (!is.null(config$wrike_task)) {
       if (config$wrike_task != "TODO") {
         rc <-
           rc |>
-          add_row(Config = "Wrike Task:", Details = config$wrike_task)
+          tibble::add_row(Config = "Wrike Task:", Details = config$wrike_task)
       }
     }
     if (!is.null(config$gitlab_repo)) {
       if (config$gitlab_repo != "TODO") {
         rc <-
           rc |>
-          add_row(Config = "Gitlab Repo:", Details = config$gitlab_repo)
+          tibble::add_row(Config = "Gitlab Repo:", Details = config$gitlab_repo)
       }
     }
     if (!is.null(config$application)) {
       if (config$application != "TODO") {
         rc <-
           rc |>
-          add_row(Config = "Application Name:", Details = config$application)
+          tibble::add_row(Config = "Application Name:", Details = config$application)
       }
     }
     if (!is.null(config$strat_col)) {
       if (config$strat_col != "TODO") {
         rc <-
           rc |>
-          add_row(Config = "Stratified By:", Details = config$strat_col)
+          tibble::add_row(Config = "Stratified By:", Details = config$strat_col)
       }
     }
     if (!is.null(config$dsn_name)) {
       if (config$dsn_name != "TODO") {
         rc <-
           rc |>
-          add_row(Config = "Data Source:", Details = config$dsn_name)
+          tibble::add_row(Config = "Data Source:", Details = config$dsn_name)
       }
     }
   } else {
@@ -78,42 +78,42 @@ report_config <- function(config = NULL, report_todo = FALSE) {
     if (!is.null(config$report_name)) {
       rc <-
         rc |>
-        add_row(Config = "Report Name:", Details = config$report_name)
+        tibble::add_row(Config = "Report Name:", Details = config$report_name)
     }
     if (!is.null(config$report_author)) {
       rc <-
         rc |>
-        add_row(Config = "Report Author:", Details = config$report_author)
+        tibble::add_row(Config = "Report Author:", Details = config$report_author)
     }
     if (!is.null(config$requested_by)) {
       rc <-
         rc |>
-        add_row(Config = "Requested By:", Details = config$requested_by)
+        tibble::add_row(Config = "Requested By:", Details = config$requested_by)
     }
     if (!is.null(config$wrike_task)) {
       rc <-
         rc |>
-        add_row(Config = "Wrike Task:", Details = config$wrike_task)
+        tibble::add_row(Config = "Wrike Task:", Details = config$wrike_task)
     }
     if (!is.null(config$gitlab_repo)) {
       rc <-
         rc |>
-        add_row(Config = "Gitlab Repo:", Details = config$gitlab_repo)
+        tibble::add_row(Config = "Gitlab Repo:", Details = config$gitlab_repo)
     }
     if (!is.null(config$application)) {
       rc <-
         rc |>
-        add_row(Config = "Application Name:", Details = config$application)
+        tibble::add_row(Config = "Application Name:", Details = config$application)
     }
     if (!is.null(config$strat_col)) {
       rc <-
         rc |>
-        add_row(Config = "Stratified By:", Details = config$strat_col)
+        tibble::add_row(Config = "Stratified By:", Details = config$strat_col)
     }
     if (!is.null(config$dsn_name)) {
       rc <-
         rc |>
-        add_row(Config = "Data Source:", Details = config$dsn_name)
+        tibble::add_row(Config = "Data Source:", Details = config$dsn_name)
     }
   }
   rc |>
